4c0e79db83b9daf6a04abd6764fd410aec8709d3
Jerffry <85122030+Jaafarkrayem@users.noreply.github.com>
2022-07-04 22:34:11 +0300

    Update BurningWallet.sol

--- BurningWallet.sol
+++ BurningWallet.sol
@@ -151,7 +151,7 @@ contract BranaBurnWallet {
     }
     function withdrawalBRANA(uint256 _amount, uint256 decimal, address to) external onlyOwner() {
         uint256 amount = BRANA.balanceOf(address(this)).sub(burnVault);
-        require(amount > 0 && amount >= _amount, "No BRANA!");// can only withdraw what is not locked for Burning.
+        require(amount > 0 && _amount <= amount, "No BRANA!");// can only withdraw what is not locked for Burning.
         uint256 dcml = 10 ** decimal;
         emit WithdrawalBRANA( _amount, decimal, to);
         BRANA.transfer(to, _amount*dcml);
